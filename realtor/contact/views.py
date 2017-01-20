26d586ad06b9746e1a3f76d1493f682cf22b20d8
Eliezer Borde <Eliezer Borde>
2017-01-19 22:11:38 -0500

    made changes in contact html and views

--- realtor/contact/views.py
+++ realtor/contact/views.py
@@ -1,4 +1,6 @@
-from django.shortcuts import render
+from django.conf import settings
+from django.core.mail import send_mail
+from django.shortcuts import render, redirect
 from django.contrib import messages
 from django.http import HttpResponseRedirect
 from .forms import ContactForm
@@ -17,8 +19,22 @@ def contact(request):
         cntct_mod.phone_number = form.cleaned_data.get("phone")
         cntct_mod.description = form.cleaned_data.get("description")
         cntct_mod.save()
+
+        subject = "User named %s %s asked a question" %(cntct_mod.firstName, cntct_mod.lastName)
+        from_email = cntct_mod.email
+        to_email = (settings.EMAIL_HOST_USER,)
+        contact_message = """
+        %s %s, via %s \n \n %s
+        """ % ( cntct_mod.firstName,cntct_mod.lastName, cntct_mod.email,cntct_mod.description )
+        send_mail(
+            subject,
+            contact_message,
+            from_email,
+            to_email,
+            fail_silently=False,
+        )
         messages.success(request, "Thank You !, I will reach you shortly...")
-        return HttpResponseRedirect("#")
+        return redirect("contact:contact")
 
     context= {
         "form":form,
